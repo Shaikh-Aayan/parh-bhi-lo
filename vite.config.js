857c5c1d31bedaf0d21a574272647fde7ed2f90a
Shaikh-Aayan <ayanmuhammad953@gmail.com>
2026-07-14 15:48:16 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/parh-bhi-lo/',    // <-- ADD THIS LINE
+  base: '/parh-bhi-lo/',
   plugins: [
     react(),
     tailwindcss()
